6980288c194668529ce70615de10763f99490bcf
bigaron <bigaron2@gmail.com>
2025-10-30 15:38:17 +0100

    Created instance creation 🥴

--- render_forge/src/Engine/Engine.cpp
+++ render_forge/src/Engine/Engine.cpp
@@ -5,6 +5,143 @@ Engine::Engine(int width, int height, const char* title): m_height(height), m_wi
 
 	// We stop glfw from creating an OpenGL context
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-
 	m_window = glfwCreateWindow(m_width, m_height, m_title, nullptr, nullptr);
+	
+	glfwSetWindowUserPointer(m_window, this);
+	glfwSetFramebufferSizeCallback(m_window, framebufferResizeCallback);
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL Engine::debugCallback(
+	VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, 
+	const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData
+){
+	std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+	return VK_FALSE;
+}
+
+VkResult Engine::create_debug_utils_messengerEXT(
+	VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* p_create_info,
+	const VkAllocationCallbacks* p_allocator, VkDebugUtilsMessengerEXT* p_debug_messenger
+){
+	auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+	if (func != nullptr) {
+		return func(instance, p_create_info, p_allocator, p_debug_messenger);
+	}
+	else {
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+}
+
+void Engine::destory_debug_utils_messengerEXT(
+	VkInstance instance, VkDebugUtilsMessengerEXT debug_messenger,
+	const VkAllocationCallbacks* p_allocator
+){
+	auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+	if (func != nullptr) { func(instance, debug_messenger, p_allocator); }
+}
+
+std::vector<char> Engine::read_file(const std::string& filename){
+	std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+	if (!file.is_open()) {
+		throw std::runtime_error("failed to find file " + filename);
+	}
+	else std::cout << "opened file:" + filename << std::endl;
+
+	size_t file_size = (size_t)file.tellg();
+	std::vector<char> buffer(file_size);
+	file.seekg(0);
+	file.read(buffer.data(), file_size);
+	file.close();
+
+	return buffer;
+}
+
+void Engine::framebufferResizeCallback(GLFWwindow* window, int width, int height){
+	auto app = reinterpret_cast<Engine*>(glfwGetWindowUserPointer(window));
+	app->m_framebuffer_resized = true;
+}
+
+void Engine::create_instance() {
+	if (enable_validation_layers && !check_validation_layer_support()) {
+		throw std::runtime_error("validation layers requested, but they are not available!");
+	}
+
+	VkApplicationInfo app_info{};
+	app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	app_info.pApplicationName = "Render_Forge";
+	app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	app_info.pEngineName = "No Engine";
+	app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	app_info.apiVersion = VK_API_VERSION_1_0;
+
+	std::vector<const char*> extensions = get_required_extensions();
+
+	VkInstanceCreateInfo create_info{};
+	create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	create_info.pApplicationInfo = &app_info;
+	create_info.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+	create_info.ppEnabledExtensionNames = extensions.data();
+
+	VkDebugUtilsMessengerCreateInfoEXT debug_create_info{};
+	if (enable_validation_layers) {
+		create_info.enabledLayerCount = static_cast<uint32_t>(validation_layers.size());
+		create_info.ppEnabledLayerNames = validation_layers.data();
+
+		populate_debug_messenger_create_info(debug_create_info);
+		create_info.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debug_create_info;
+	}
+	else {
+		create_info.enabledLayerCount = 0;
+	}
+
+	if (vkCreateInstance(&create_info, nullptr, &m_instance) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create vulkan instance!");
+	}
+}
+
+bool Engine::check_validation_layer_support() const{
+	uint32_t layer_count;
+	vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
+	std::vector<VkLayerProperties> available_layers(layer_count);
+	vkEnumerateInstanceLayerProperties(&layer_count, available_layers.data());
+
+	for (const char* layer_name : validation_layers) {
+		bool layer_found = false;
+
+		for (const auto& layer_properties : available_layers) {
+			if (strcmp(layer_name, layer_properties.layerName) == 0) {
+				layer_found = true;
+				break;
+			}
+		}
+
+		if (!layer_found) return false;
+	}
+
+	return true;
+}
+
+std::vector<const char*> Engine::get_required_extensions() const {
+	uint32_t glfw_extension_count = 0;
+	const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+	std::vector<const char*> extensions(glfw_extensions, glfw_extension_count + glfw_extensions);
+	if (enable_validation_layers) { extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME); }
+
+	return extensions;
+}
+
+void Engine::populate_debug_messenger_create_info(VkDebugUtilsMessengerCreateInfoEXT& create_info) {
+	create_info = {};
+	create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+
+	create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	create_info.pfnUserCallback = debugCallback;
+	create_info.pUserData = nullptr;
 }
--- render_forge/src/Engine/Engine.h
+++ render_forge/src/Engine/Engine.h
@@ -11,14 +11,52 @@
 #include <glm/gtc/matrix_transform.hpp>
 #pragma warning(pop)
 
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include <fstream>
+
+
+const std::vector<const char*> validation_layers = { "VK_LAYER_KHRONOS_validation" };
+const std::vector<const char*> device_extensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+const int MAX_FRAMES_IN_FLIGHT = 2;
+
+#ifdef NDEBUG
+const bool enable_validation_layers = false;
+#else
+const bool enable_validation_layers = true;
+#endif
+
+
 
 class Engine {
 public:
 	Engine(int width, int height, const char* title);
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+        void* pUserData);
+    static VkResult create_debug_utils_messengerEXT(VkInstance instance,
+        const VkDebugUtilsMessengerCreateInfoEXT* p_create_info,
+        const VkAllocationCallbacks* p_allocator,
+        VkDebugUtilsMessengerEXT* p_debug_messenger);
+    static void destory_debug_utils_messengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debug_messenger,
+        const VkAllocationCallbacks* p_allocator);
+    static std::vector<char> read_file(const std::string& filename);
+    static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
 private:
 	GLFWwindow* m_window;
 	const char* m_title;
 	const int m_width;
 	const int m_height;
+    bool m_framebuffer_resized = false;
+    VkInstance m_instance = VK_NULL_HANDLE;
+
 	Engine() = delete;
+    void create_instance();
+    bool check_validation_layer_support() const;
+    std::vector<const char*> get_required_extensions() const;
+    void populate_debug_messenger_create_info(VkDebugUtilsMessengerCreateInfoEXT& create_info);
 };
