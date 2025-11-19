7006382dca6e7ce8f6b33c6cd51e6cd15a9d2705
bigaron <bigaron2@gmail.com>
2025-11-19 23:31:22 +0100

    added assimp to the project

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -1,6 +1,6 @@
 #include "first_app.hpp"
-#include "frg_camera.hpp"
 
+#include "frg_camera.hpp"
 #include "simple_render_system.hpp"
 
 // libs
@@ -10,117 +10,102 @@
 #include <glm/gtc/constants.hpp>
 
 // std
-#include <stdexcept>
 #include <array>
 #include <cassert>
+#include <stdexcept>
 
-namespace frg
-{
-    FirstApp::FirstApp()
-    {
-        loadGameObjects();
-    }
+namespace frg {
+FirstApp::FirstApp() { loadGameObjects(); }
 
-    FirstApp::~FirstApp() {}
+FirstApp::~FirstApp() {}
 
-    void FirstApp::run()
-    {
-        SimpleRenderSystem simpleRenderSystem{frgDevice, frgRenderer.getSwapChainRenderPass()};
-        FrgCamera camera{};
-        // example camera setup
-        camera.setViewTarget(
-            glm::vec3{-1.f, -2.f, 2.f},
-            glm::vec3{0.f, 0.f, 2.5f});
+void FirstApp::run() {
+    SimpleRenderSystem simpleRenderSystem{frgDevice, frgRenderer.getSwapChainRenderPass()};
+    FrgCamera camera{};
+    // example camera setup
+    camera.setViewTarget(glm::vec3{-1.f, -2.f, 2.f}, glm::vec3{0.f, 0.f, 2.5f});
 
-        while (!frgWindow.shouldClose())
-        {
-            glfwPollEvents();
-            float aspect = frgRenderer.getAspectRatio();
-            // camera.setOrthographicProjection(-aspect, aspect, -1, 1, -1, 1);
-            camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 10.f);
+    while (!frgWindow.shouldClose()) {
+        glfwPollEvents();
+        float aspect = frgRenderer.getAspectRatio();
+        // camera.setOrthographicProjection(-aspect, aspect, -1, 1, -1, 1);
+        camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 10.f);
 
-            if (auto commandBuffer = frgRenderer.beginFrame())
-            {
-                // make the render passes here, for example: shadow pass, post processing pass, etc.
-                frgRenderer.beginSwapChainRenderPass(commandBuffer);
-                simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects, camera);
+        if (auto commandBuffer = frgRenderer.beginFrame()) {
+            // make the render passes here, for example: shadow pass, post processing pass, etc.
+            frgRenderer.beginSwapChainRenderPass(commandBuffer);
+            simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects, camera);
 
-                frgRenderer.endSwapChainRenderPass(commandBuffer);
-                frgRenderer.endFrame();
-            }
+            frgRenderer.endSwapChainRenderPass(commandBuffer);
+            frgRenderer.endFrame();
         }
-
-        vkDeviceWaitIdle(frgDevice.device());
-    }
-    // temporary helper function, creates a 1x1x1 cube centered at offset
-    std::unique_ptr<FrgModel> createCubeModel(FrgDevice &device, glm::vec3 offset)
-    {
-        std::vector<FrgModel::Vertex> vertices{
-
-            // left face (white)
-            {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
-            {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
-            {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
-            {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
-            {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
-            {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
-
-            // right face (yellow)
-            {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
-            {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
-            {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
-            {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
-            {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
-            {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
-
-            // top face (orange, remember y axis points down)
-            {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-            {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-            {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-            {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-            {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-            {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-
-            // bottom face (red)
-            {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-            {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
-            {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
-            {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-            {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-            {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
-
-            // nose face (blue)
-            {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-            {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-            {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-            {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-            {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-            {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-
-            // tail face (green)
-            {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-            {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-            {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-            {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-            {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-            {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-
-        };
-        for (auto &v : vertices)
-        {
-            v.position += offset;
-        }
-        return std::make_unique<FrgModel>(device, vertices);
     }
 
-    void FirstApp::loadGameObjects()
-    {
-        std::shared_ptr<FrgModel> cubeModel = createCubeModel(frgDevice, {0.f, 0.f, 0.f});
-        auto cube = FrgGameObject::createGameObject();
-        cube.model = cubeModel;
-        cube.transform.translation = {0.f, 0.f, 2.5f};
-        cube.transform.scale = {0.5f, 0.5f, 0.5f};
-        gameObjects.push_back(std::move(cube));
-    }
+    vkDeviceWaitIdle(frgDevice.device());
+}
+// temporary helper function, creates a 1x1x1 cube centered at offset
+std::unique_ptr<FrgModel> createCubeModel(FrgDevice &device, glm::vec3 offset) {
+    std::vector<FrgModel::Vertex> vertices{
 
-} // namespace frg
+        // left face (white)
+        { {-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+        {   {-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+        {  {-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+        { {-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+        {  {-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+        {   {-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+
+        // right face (yellow)
+        {  {.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+        {    {.5f, .5f, .5f}, {.8f, .8f, .1f}},
+        {   {.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+        {  {.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+        {   {.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+        {    {.5f, .5f, .5f}, {.8f, .8f, .1f}},
+
+        // top face (orange, remember y axis points down)
+        { {-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+        {   {.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+        {  {-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+        { {-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+        {  {.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+        {   {.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+
+        // bottom face (red)
+        {  {-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+        {    {.5f, .5f, .5f}, {.8f, .1f, .1f}},
+        {   {-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+        {  {-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+        {   {.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+        {    {.5f, .5f, .5f}, {.8f, .1f, .1f}},
+
+        // nose face (blue)
+        { {-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+        {   {.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+        {  {-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+        { {-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+        {  {.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+        {   {.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+
+        // tail face (green)
+        {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+        {  {.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+        { {-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+        {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+        { {.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+        {  {.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+    };
+    for (auto &v : vertices) { v.position += offset; }
+    return std::make_unique<FrgModel>(device, vertices);
+}
+
+void FirstApp::loadGameObjects() {
+    std::shared_ptr<FrgModel> cubeModel = createCubeModel(frgDevice, {0.f, 0.f, 0.f});
+    auto cube = FrgGameObject::createGameObject();
+    cube.model = cubeModel;
+    cube.transform.translation = {0.f, 0.f, 2.5f};
+    cube.transform.scale = {0.5f, 0.5f, 0.5f};
+    gameObjects.push_back(std::move(cube));
+}
+
+}  // namespace frg
--- src/first_app.hpp
+++ src/first_app.hpp
@@ -1,37 +1,38 @@
 #pragma once
 
+#include "assimp/importer.hpp"
+#include "assimp/postprocess.h"
 #include "frg_device.hpp"
 #include "frg_game_object.hpp"
-#include "frg_window.hpp"
 #include "frg_renderer.hpp"
+#include "frg_window.hpp"
 
 // std
 #include <memory>
+#include <string>
 #include <vector>
 
-namespace frg
-{
-    class FirstApp
-    {
-    public:
-        static constexpr int WIDTH = 800;
-        static constexpr int HEIGHT = 600;
+namespace frg {
+class FirstApp {
+   public:
+    static constexpr int WIDTH = 800;
+    static constexpr int HEIGHT = 600;
 
-        FirstApp();
-        ~FirstApp();
+    FirstApp();
+    ~FirstApp();
 
-        FirstApp(const FirstApp &) = delete;
-        FirstApp &operator=(const FirstApp &) = delete;
+    FirstApp(const FirstApp &) = delete;
+    FirstApp &operator=(const FirstApp &) = delete;
 
-        void run();
+    void run();
 
-    private:
-        void loadGameObjects();
+   private:
+    void loadGameObjects();
 
-        FrgWindow frgWindow{WIDTH, HEIGHT, "MERHABA VULKAN!"};
-        FrgDevice frgDevice{frgWindow};
-        FrgRenderer frgRenderer{frgWindow, frgDevice};
+    FrgWindow frgWindow{WIDTH, HEIGHT, "MERHABA VULKAN!"};
+    FrgDevice frgDevice{frgWindow};
+    FrgRenderer frgRenderer{frgWindow, frgDevice};
 
-        std::vector<FrgGameObject> gameObjects;
-    };
-} // namespace frg
+    std::vector<FrgGameObject> gameObjects;
+};
+}  // namespace frg
