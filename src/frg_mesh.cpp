be8adb37c203912a758e0cf8777ba1b94b773e04
bigaron <bigaron2@gmail.com>
2025-11-20 16:59:15 +0100

    fixed vertex attribute description error

--- src/frg_mesh.cpp
+++ src/frg_mesh.cpp
@@ -182,10 +182,10 @@ std::array<VkVertexInputAttributeDescription, 3>
     attribute_descriptions[1].offset = offsetof(Vertex, normal);
 
     // Texture coord attribute
-    attribute_descriptions[0].binding = 0;
-    attribute_descriptions[0].location = 2;
-    attribute_descriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
-    attribute_descriptions[0].offset = offsetof(Vertex, tex_coord);
+    attribute_descriptions[2].binding = 0;
+    attribute_descriptions[2].location = 2;
+    attribute_descriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+    attribute_descriptions[2].offset = offsetof(Vertex, tex_coord);
 
     return attribute_descriptions;
 }
