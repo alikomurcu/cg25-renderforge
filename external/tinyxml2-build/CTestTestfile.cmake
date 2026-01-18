f42365e2b8e6c670d64fcc465e74563c3541fdb1
bigaron <bigaron2@gmail.com>
2026-01-18 20:16:03 +0100

    externals

---
+++ external/tinyxml2-build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/bigaron/programming/egyetem/computer_graphics/final/cg25-renderforge/external/tinyxml2-src
+# Build directory: C:/Users/bigaron/programming/egyetem/computer_graphics/final/cg25-renderforge/external/tinyxml2-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(xmltest "C:/Users/bigaron/programming/egyetem/computer_graphics/final/cg25-renderforge/build/xmltest.exe")
+set_tests_properties(xmltest PROPERTIES  PASS_REGULAR_EXPRESSION ", Fail 0" WORKING_DIRECTORY "C:/Users/bigaron/programming/egyetem/computer_graphics/final/cg25-renderforge/external/tinyxml2-src" _BACKTRACE_TRIPLES "C:/Users/bigaron/programming/egyetem/computer_graphics/final/cg25-renderforge/external/tinyxml2-src/CMakeLists.txt;49;add_test;C:/Users/bigaron/programming/egyetem/computer_graphics/final/cg25-renderforge/external/tinyxml2-src/CMakeLists.txt;0;")
